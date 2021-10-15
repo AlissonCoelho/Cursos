a369168e0aad64f977415def0c367e461608ded5
Alisson <alissonahc@gmail.com>
2021-10-15 12:18:26 -0300

    alterado arquivo Objetos.js

--- Curso JavaScript/Objetos.js
+++ Curso JavaScript/Objetos.js
@@ -101,7 +101,7 @@ const cliente3 =
     CPF: "11544798926",
     idade: 28,
     email: "alissonahc@gmail.com",
-    fone: ["031971270466", "31922262516"],
+    fone: [31971270466, 31922262516],
     dependentes:
     [{
         parentesco: "filha",
@@ -123,7 +123,7 @@ const cliente3 =
 }
 
 cliente3.depositar(30);
-console.log(cliente3);
+//console.log(cliente3);
 
 //--------------------------------------
 //Informações sobre objetos
@@ -137,8 +137,8 @@ const objPersonagem = {
 const objPersonagem2 = objPersonagem
 objPersonagem2.nome = "Gandalf, o Cinzento"
 
-console.log(objPersonagem.nome) //Gandalf, o Cinzento
-console.log(objPersonagem2.nome) //Gandalf, o Cinzento
+//console.log(objPersonagem.nome) //Gandalf, o Cinzento
+//console.log(objPersonagem2.nome) //Gandalf, o Cinzento
 
 //A variável objPersonagem2 não fez uma cópia do objeto original,
 //apenas serviu como referência para o objeto original objPersonagem.
@@ -146,19 +146,85 @@ console.log(objPersonagem2.nome) //Gandalf, o Cinzento
 //Isso porque o JavaScript, quando trabalha com objetos,
 //acessa os valores deles fazendo referência ao original. mas não cria uma cópia.
 //Já o acesso por cópia funciona com tipos primitivos (string, number, booleano, null, symbol):
-const objPersonagem = {
+objPersonagem3 = {
     nome: "Gandalf",
     classe: "mago",
     nivel: "20"
    }
    
-   const objPersonagem2 = Object.create(objPersonagem)
-   objPersonagem2.nome = "Gandalf, o Cinzento"
    
-   console.log(objPersonagem.nome) //Gandalf
-   console.log(objPersonagem2.nome) //Gandalf, o Cinze
+objPersonagem4 = Object.create(objPersonagem3)
+objPersonagem4.nome = "Gandalf, o Cinzento"
+   
+//console.log(objPersonagem3.nome) //Gandalf
+//console.log(objPersonagem4.nome) //Gandalf, o Cinze
 
-   //O método Object.create() cria um novo objeto utilizando como protótipo
-   //o objeto passado via parâmetro. Dessa forma, objPersonagem2 é uma instância
-   //diferente de objPersonagem e pode ser trabalhada de forma independente.
+//O método Object.create() cria um novo objeto utilizando como protótipo
+//o objeto passado via parâmetro. Dessa forma, objPersonagem2 é uma instância
+//diferente de objPersonagem e pode ser trabalhada de forma independente.
 
+
+//--------------------------------------
+//Percorrer objetos - For ...in
+//--------------------------------------
+
+let relatorio = "";
+
+for (let info in cliente3)
+{
+    if(typeof cliente3[info] != "function")
+    {
+        if (typeof cliente3[info] != "object")
+        {
+            relatorio += `${info}: ${cliente3[info]}\n`;
+        }
+    }
+    
+}
+//console.log(typeof cliente3.fone);
+//console.log(relatorio);
+
+//----------------------------------------------------------------
+//Criar um array com as chaves do objetos - Object.keys(objeto)
+//----------------------------------------------------------------
+
+const propsClient3 = Object.keys(cliente3);
+//console.log(propsClient3);
+
+const Mariz =[0,1,2]
+const propsMatriz = Object.keys(Mariz);
+//console.log(propsMatriz);
+
+const oferecerSeguro = obj =>  {
+    if (Object.keys(obj).includes("dependentes")){
+        console.log("Oferecer seguro para: " + obj.nome);
+    }
+}
+
+//oferecerSeguro(cliente3);
+
+const testFilter = Object.keys(cliente3).filter(valor => valor ==="dependentes");
+//console.log(testFilter);
+
+//----------------------------------------------------------------
+//Criar um array com os valores das chaves - Object.values(objeto)
+//----------------------------------------------------------------
+const valoresClient3 = Object.values(cliente3);
+//console.log(valoresClient3);
+
+//----------------------------------------------------------------
+//Criar um array com os valores e chaves - Object.entries(objeto)
+//----------------------------------------------------------------
+
+const entriesClient3 = Object.entries(cliente3);
+//console.log(entriesClient3);
+// [
+//     [ 'nome', 'Alisson' ],
+//     [ 'CPF', '11544798926' ],
+//     [ 'idade', 28 ],
+//     [ 'email', 'alissonahc@gmail.com' ],
+//     [ 'fone', [ 31971270466, 31922262516] ],
+//     [ 'dependentes', [ [Object], [Object] ] ],
+//     [ 'saldo', 130 ],
+//     [ 'depositar', [Function: depositar]]
+// ]
