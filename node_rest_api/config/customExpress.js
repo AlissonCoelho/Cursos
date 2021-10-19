8ac6b7bd40d9c17cc69cd167be39d2618a7b732b
Alisson <alissonahc@gmail.com>
2021-10-19 13:16:40 -0300

    Aula: Enviar dados relativos ao pagamento, recebendo post

--- node_rest_api/config/customExpress.js
+++ node_rest_api/config/customExpress.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const consign = require('consign')
+//const bodyParser = require('body-parser')
 
 //Exporta
 module.exports = () =>
 {
-    const app = express();
+    const app = express()
+
+    //app.use(bodyParser.json())
+    app.use(express.json()) 
+
+    //app.use(bodyParser.urlencoded({extended:true}))
+    app.use(express.urlencoded())
+    
     //"enxerga" a pasta controllers e o que foi exportado de lá
     consign()
         .include('controllers')
--- node_rest_api/controllers/atendimentos.js
+++ node_rest_api/controllers/atendimentos.js
@@ -5,5 +5,10 @@ module.exports = app =>
     //req = recebido
     //res = enviando
     app.get('/atendimentos',(req, res) => res.send(`Você esta na rota atendimentos adicionado`))
-    app.post('/atendimentos',(req, res) => res.send(`Você está na rota de atendimento e está realizando um POST`) )
+
+    app.post('/atendimentos',(req, res) => {
+        console.log(req.body)
+        res.send(`Você está na rota de atendimento e está realizando um POST`)
+    }
+    )
 }
