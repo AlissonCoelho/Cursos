650391fa6fedc0fc3ef20e9e1df1f22be414056b
Alisson <alissonahc@gmail.com>
2021-11-11 16:10:43 -0300

    feito CRUD em na tabela Pessoas utilizando os metodos HTTP = {pots: Criar, get: buscar, put: atualizar, delete: apagar}

--- ORM_Sequelize/api/controllers/Pessoa.js
+++ ORM_Sequelize/api/controllers/Pessoa.js
@@ -4,9 +4,74 @@ class Pessoa {
   static async getAllPeople(req, res) {
     try {
       const allPeople = await dataBase.Pessoas.findAll();
+
+      console.log("pessoas encontradas");
       return res.status(200).json(allPeople);
     } catch (error) {
-        return res.status(500).json(error.message);
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async getOnePerson(req, res) {
+    try {
+      const { id } = req.params;
+      const person = await dataBase.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      console.log("pessoa encontrada id:", person.id);
+      return res.status(200).json(person);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async createPerson(req, res) {
+    try {
+      const person = req.body;
+      const newPerson = await dataBase.Pessoas.create(person);
+      console.log("pessoa criada id:", newPerson.id);
+      return res.status(200).json(newPerson);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  static async updataPerson(req, res) {
+    try {
+      const { id } = req.params;
+      const dados = req.body;
+      const updatePerson = await dataBase.Pessoas.update(dados, {where: {id: Number(id)}});
+      if(updatePerson)
+      {
+        console.log("pessoa atualizada id:", id);
+        return res.status(200).send(`pessoa atualizada id: ${id}`);
+      }
+      else
+      {
+        console.log("pessoa não atualizada id:", id);
+        return res.status(203).send("Não atualizado");
+      }
+      
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  static async deletePerson(req, res) {
+    try {
+      const { id } = req.params;
+      const deletePerson = await dataBase.Pessoas.destroy({where: {id: Number(id)}});
+      if(deletePerson)
+      {
+        console.log("pessoa deletada id:", id);
+        return res.status(200).send(`pessoa deletada id: ${id}`);
+      }
+      else
+      {
+        console.log("pessoa não deletada id:", id);
+        return res.status(203).send("Não deletada");
+      }
+      
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
   }
 }
--- ORM_Sequelize/api/routes/pessoas.js
+++ ORM_Sequelize/api/routes/pessoas.js
@@ -5,4 +5,12 @@ const router = Router();
 
 router.get("/pessoas", PessoaControllers.getAllPeople);
 
+router.get("/pessoas/:id", PessoaControllers.getOnePerson);
+
+router.post("/pessoas/", PessoaControllers.createPerson);
+
+router.put("/pessoas/:id", PessoaControllers.updataPerson);
+
+router.delete("/pessoas/:id", PessoaControllers.deletePerson);
+
 module.exports = router;
